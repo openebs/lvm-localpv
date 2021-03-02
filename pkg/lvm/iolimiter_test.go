4f1c9a08f059d8e44f2a3ec7611e0f30a6358de2
abhranil.chatterjee <abhranil.chatterjee@flipkart.com>
2021-03-02 12:54:12 +0530

    ran go fmt on iolimiter_test

--- pkg/lvm/iolimiter_test.go
+++ pkg/lvm/iolimiter_test.go
@@ -21,7 +21,7 @@ import (
 	"github.com/openebs/lvm-localpv/pkg/config"
 )
 
-func TestExtractingIOLimits(t *testing.T) {
+func TestExtractingIoLimits(t *testing.T) {
 	//beforeFunc := func(value string) {
 	//	if err := os.Setenv(string(OpenEBSPingPeriod), value); err != nil {
 	//		t.Logf("Unable to set environment variable")
@@ -35,26 +35,26 @@ func TestExtractingIOLimits(t *testing.T) {
 	type expectedIoLimitRate struct {
 		riops uint64
 		wiops uint64
-		rbps uint64
-		wbps uint64
+		rbps  uint64
+		wbps  uint64
 	}
 	testSuite := map[string]struct {
-		config *config.Config
-		vgNames *[]string
+		config   *config.Config
+		vgNames  *[]string
 		expected map[string]expectedIoLimitRate
 	}{
 		"Riops and Wiops": {
 			config: &config.Config{
-				SetIOLimits:true,
-				RIopsLimitPerGB:  &[]string{"lvmvg1:50", "lvmvg2:100"},
-				WIopsLimitPerGB:  &[]string{"lvmvg1:70", "lvmvg2:120"},
-				RBpsLimitPerGB:   nil,
-				WBpsLimitPerGB:   nil,
+				SetIOLimits:     true,
+				RIopsLimitPerGB: &[]string{"lvmvg1:50", "lvmvg2:100"},
+				WIopsLimitPerGB: &[]string{"lvmvg1:70", "lvmvg2:120"},
+				RBpsLimitPerGB:  nil,
+				WBpsLimitPerGB:  nil,
 			}, vgNames: &[]string{"lvmvg1-id1", "lvmvg2", "lvmvg3"},
 			expected: map[string]expectedIoLimitRate{
 				"lvmvg1-id1": {riops: 50, wiops: 70, rbps: 0, wbps: 0},
-				"lvmvg2": {riops: 100, wiops: 120, rbps: 0, wbps: 0},
-				"lvmvg3": {riops: 0, wiops: 0, rbps: 0, wbps: 0},
+				"lvmvg2":     {riops: 100, wiops: 120, rbps: 0, wbps: 0},
+				"lvmvg3":     {riops: 0, wiops: 0, rbps: 0, wbps: 0},
 			},
 		},
 	}
@@ -68,4 +68,3 @@ func TestExtractingIOLimits(t *testing.T) {
 		}
 	}
 }
-
