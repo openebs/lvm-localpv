63094c17e05b44ab97dc5fc94be72042ff7407fe
Sahil Raja <sahilraja242@gmail.com>
2021-06-24 11:16:42 +0530

    Minor fix
    Signed-off-by: Sahil Raja <sahilraja242@gmail.com>

--- pkg/lvm/lvm_util.go
+++ pkg/lvm/lvm_util.go
@@ -66,12 +66,12 @@ type LogicalVolume struct {
 	Name string
 
 	// TODO
-	// FullName is not getting populated currently. We will have to populate it for using it as labels in lvm metrics.
+	// FullName is not getting populated currently. We will have to populate it for using it as a label in lvm metrics.
 	// Full name of the lvm logical volume (fullName: linuxlvmvg/pvc-213ca1e6-e271-4ec8-875c-c7def3a4908d)
 	FullName string
 
 	// TODO
-	// UUID is not getting populated currently. We will have to populate it for using it as labels in lvm metrics.
+	// UUID is not getting populated currently. We will have to populate it for using it as a label in lvm metrics.
 	// UUID denotes a unique identity of a lvm logical volume.
 	UUID string
 
